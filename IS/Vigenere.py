cdc33b6e0c52d65e77568af794bab4c66153e9da
Varun Viswanath <varunvis2903@gmail.com>
2024-05-12 13:31:36 +0530

    now with decryption

--- IS/Vigenere.py
+++ IS/Vigenere.py
@@ -3,13 +3,15 @@ text = input("Enter the plaintext: ").upper()
 key = input("Enter the key: ").upper()
         
 while len(key) != len(text):
-    if len(text) > 2*len(text)-1:
-        key += key
-    else:
-        key += key[:len(text)-len(key)]
+    key += key[:len(text)-len(key)]
 
 encrypt = ""
 for i in range(len(text)):
     encrypt += chr((ord(text[i]) + ord(key[i]))%26 + 65)
 
-print(encrypt)
+decrypt = ""
+for i in range(len(encrypt)):
+    decrypt += chr((ord(encrypt[i]) - ord(key[i]))%26 + 65)
+
+print(f"Encrypted value: {encrypt}")
+print(f"Decrypted value: {decrypt}")
